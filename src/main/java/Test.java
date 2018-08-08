77a0688e9829643c93719e88852135e877f23b74
huod <huod247@163.com>
2018-08-08 22:44:32 +0800

    分支提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,5 +8,7 @@ class Test {
          System.out.println("远程仓库进行的修改");
         System.out.println("第三次修改");
         System.out.println("我是dev");
+        System.out.println("我是dev");
+        System.out.println("我是dev");
     }
 }
